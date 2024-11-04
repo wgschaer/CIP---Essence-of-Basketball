4464b76b62278765ce04af9c7ccd81205ee1f5ce
pstylo <shabin.poon@gmail.com>
2024-11-04 17:54:17 +0100

    NBA MVP and Championship list from 1999 - 2023

---
+++ Sabin/03  Data Cleaning mvp.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+
+# Load the data from CSV files
+mvp_data = pd.read_csv("D:/MDS 2024/2nd Semester/CIP02 Data Collection, Integration and Preprocessing/CIP---Essence-of-Basketball/Sabin/nba_mvp_data_1999_2024.csv")
+
+#"D:\MDS 2024\2nd Semester\CIP02 Data Collection, Integration and Preprocessing\CIP---Essence-of-Basketball\Sabin\nba_champions_1999_2024.csv"
+
+
+champion_data = pd.read_csv("/Sabin/Data/nba_champions_1999_2024.csv")
+
+# Display the first few rows of each DataFrame
+print(mvp_data.head())
+print(champion_data.head())
