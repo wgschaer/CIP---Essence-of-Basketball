81412bdd919169fd1c9c1de37691984e10e32297
pstylo <shabin.poon@gmail.com>
2024-11-10 22:48:08 +0100

    NBA MVP and Championship list from 1999 - 2023

--- Sabin/02  Data Cleaning mvp.py
+++ Sabin/02  Data Cleaning mvp.py
@@ -13,16 +13,16 @@ mvp_df.rename(columns={'Season': 'Year'}, inplace=True)
 merged_df = pd.merge(mvp_df, champions_df, on='Year', how='inner')
 
 # Step 4: Drop unnecessary columns
-columns_to_drop = ['Lg', 'Voting', 'League', 'Points Leader', 'Rebounds Leader',
-                   'Assists Leader', 'Win Shares Leader']
+columns_to_drop = ['Lg', 'Voting', 'League', 'Points Leader', 'Rebounds Leader', 'Assists Leader', 'Win Shares Leader']
 merged_df.drop(columns=columns_to_drop, inplace=True)
 
 # Step 5: Clean player names and finals MVP names using `unidecode` to remove special characters
 merged_df['Player'] = merged_df['Player'].apply(lambda x: unidecode(x) if isinstance(x, str) else x)
 merged_df['Finals MVP'] = merged_df['Finals MVP'].apply(lambda x: unidecode(x) if isinstance(x, str) else x)
 
-# Step 6: Rename 'Player' to 'Season MVP' and format names
+# Step 6: Rename 'Player' to 'Season MVP' and format names and
 merged_df.rename(columns={'Player': 'Season MVP'}, inplace=True)
+merged_df.rename(columns={'Champion': 'Champion Team'}, inplace=True)
 
 # Function to format names to "F. Last"
 def format_name(name):
@@ -36,7 +36,7 @@ def format_name(name):
 merged_df['Season MVP'] = merged_df['Season MVP'].apply(format_name)
 
 # Step 7: Rename 'Tm' to 'Team' and convert abbreviations to full team names
-merged_df.rename(columns={'Tm': 'Team'}, inplace=True)
+merged_df.rename(columns={'Tm': 'MVP Team'}, inplace=True)
 
 # Dictionary to map team abbreviations to full names
 team_mapping = {
@@ -51,13 +51,18 @@ team_mapping = {
 }
 
 # Apply the team mapping
-merged_df['Team'] = merged_df['Team'].map(team_mapping)
+merged_df['MVP Team'] = merged_df['MVP Team'].map(team_mapping)
 
-# Step 8: Display the final cleaned data
+# Step 8: Convert columns 'FG%', '3P%', and 'FT%' to percentages
+percentage_columns = ['FG%', '3P%', 'FT%']
+for col in percentage_columns:
+    merged_df[col] = merged_df[col].apply(lambda x: f"{round(x * 100)}%" if pd.notnull(x) else x)
+
+# Step 9: Display the final cleaned data
 print("\nCleaned Merged Data - First 5 Rows:")
-print(merged_df[['Year', 'Season MVP', 'Team', 'Champion', 'Finals MVP', 'PTS', 'TRB', 'AST']].head())
+print(merged_df[['Year', 'Season MVP', 'MVP Team', 'Champion Team', 'Finals MVP', 'PTS', 'TRB', 'AST', 'FG%', '3P%', 'FT%']].head())
 
-# Step 9: Save the cleaned data to a new CSV file
-final_output_path = 'data/cleaned_nba_data_final_with_teams.csv'
+# Step 10: Save the cleaned data to a new CSV file
+final_output_path = 'data/cleaned_nba_data_mvp_with_teams.csv'
 merged_df.to_csv(final_output_path, index=False)
 print(f"\nCleaned data saved to '{final_output_path}'")
