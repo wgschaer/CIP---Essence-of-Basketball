0a21c2f9d8bdf344d306548dac4b3dc2d5548462
pstylo <shabin.poon@gmail.com>
2024-11-10 18:23:22 +0100

    NBA MVP and Championship list from 1999 - 2023

--- Sabin/02  Data Cleaning mvp.py
+++ Sabin/02  Data Cleaning mvp.py
@@ -17,7 +17,7 @@ columns_to_drop = ['Lg', 'Voting', 'League', 'Points Leader', 'Rebounds Leader',
                    'Assists Leader', 'Win Shares Leader']
 merged_df.drop(columns=columns_to_drop, inplace=True)
 
-# Step 5: Clean player names and finals MVP names using `unidecode`
+# Step 5: Clean player names and finals MVP names using `unidecode` to remove special characters
 merged_df['Player'] = merged_df['Player'].apply(lambda x: unidecode(x) if isinstance(x, str) else x)
 merged_df['Finals MVP'] = merged_df['Finals MVP'].apply(lambda x: unidecode(x) if isinstance(x, str) else x)
 
@@ -40,36 +40,12 @@ merged_df.rename(columns={'Tm': 'Team'}, inplace=True)
 
 # Dictionary to map team abbreviations to full names
 team_mapping = {
-    'ATL': 'Atlanta Hawks',
-    'BOS': 'Boston Celtics',
-    'BKN': 'Brooklyn Nets',
-    'CHA': 'Charlotte Hornets',
-    'CHI': 'Chicago Bulls',
-    'CLE': 'Cleveland Cavaliers',
-    'DAL': 'Dallas Mavericks',
-    'DEN': 'Denver Nuggets',
-    'DET': 'Detroit Pistons',
-    'GSW': 'Golden State Warriors',
-    'HOU': 'Houston Rockets',
-    'IND': 'Indiana Pacers',
-    'LAC': 'Los Angeles Clippers',
-    'LAL': 'Los Angeles Lakers',
-    'MEM': 'Memphis Grizzlies',
-    'MIA': 'Miami Heat',
-    'MIL': 'Milwaukee Bucks',
-    'MIN': 'Minnesota Timberwolves',
-    'NOP': 'New Orleans Pelicans',
-    'NYK': 'New York Knicks',
-    'OKC': 'Oklahoma City Thunder',
-    'ORL': 'Orlando Magic',
-    'PHI': 'Philadelphia 76ers',
-    'PHO': 'Phoenix Suns',
-    'POR': 'Portland Trail Blazers',
-    'SAC': 'Sacramento Kings',
-    'SAS': 'San Antonio Spurs',
-    'TOR': 'Toronto Raptors',
-    'UTA': 'Utah Jazz',
-    'WAS': 'Washington Wizards'
+    'ATL': 'Atlanta Hawks','BOS': 'Boston Celtics','BKN': 'Brooklyn Nets', 'CHA': 'Charlotte Hornets','CHI': 'Chicago Bulls', 'CLE': 'Cleveland Cavaliers',
+    'DAL': 'Dallas Mavericks','DEN': 'Denver Nuggets','DET': 'Detroit Pistons','GSW': 'Golden State Warriors','HOU': 'Houston Rockets','IND': 'Indiana Pacers',
+    'LAC': 'Los Angeles Clippers','LAL': 'Los Angeles Lakers','MEM': 'Memphis Grizzlies','MIA': 'Miami Heat','MIL': 'Milwaukee Bucks','MIN': 'Minnesota Timberwolves',
+    'NOP': 'New Orleans Pelicans','NYK': 'New York Knicks','OKC': 'Oklahoma City Thunder','ORL': 'Orlando Magic','PHI': 'Philadelphia 76ers',
+    'PHO': 'Phoenix Suns','POR': 'Portland Trail Blazers','SAC': 'Sacramento Kings',
+    'SAS': 'San Antonio Spurs','TOR': 'Toronto Raptors', 'UTA': 'Utah Jazz','WAS': 'Washington Wizards'
 }
 
 # Apply the team mapping
